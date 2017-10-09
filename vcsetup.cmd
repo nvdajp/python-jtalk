a9010bc551665cca06ebd874bafaf1dd28804588
Takuya Nishimoto <nishimotz@gmail.com>
2017-10-09 18:25:20 +0900

    use Visual Studio 2017 Community

--- vcsetup.cmd
+++ vcsetup.cmd
@@ -2,10 +2,16 @@
 cl
 if "%ERRORLEVEL%" neq "9009" goto :done
 
-if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat" goto x64
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat" goto vc2017x64
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat" goto vc2015x64
 call "C:\Program Files\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
-goto done
-:x64
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
-:done
 SET CL=/arch:IA32 /D "_USING_V110_SDK71_"
+goto done
+:vc2015x64
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
+SET CL=/arch:IA32 /D "_USING_V110_SDK71_"
+goto done
+:vc2017x64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
+SET CL=/arch:IA32
+:done
