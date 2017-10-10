c6d7b22ba10c631f2b6904ae94dbcb8b6fc6304a
Takuya Nishimoto <nishimotz@gmail.com>
2017-10-11 00:09:47 +0900

    Python 3 support in progress

--- lib/make_timestamp.py
+++ lib/make_timestamp.py
@@ -1,6 +1,13 @@
+import sys
+if sys.version_info[0] > 2:
+	open_file  = lambda name, mode, encoding : open(name, mode, encoding=encoding)
+	encode_str = lambda s, encoding : s
+else:
+	open_file  = lambda name, mode, encoding : open(name, mode)
+	encode_str = lambda s, encoding : s.encode(encoding)
 import os
 import datetime
 s = "libopenjtalk " + datetime.datetime.utcnow().strftime('%Y%m%d-%H%M%S')
-with open("libopenjtalk-timestamp.h", "wb") as f:
-	f.write("#define JT_VERSION \"%s\"" % s)
-	f.write(os.linesep)
+with open_file("libopenjtalk-timestamp.h", "w", 'utf-8') as f:
+	f.write(encode_str(("#define JT_VERSION \"%s\"" % s), 'utf-8'))
+	f.write(encode_str(os.linesep, 'utf-8'))
