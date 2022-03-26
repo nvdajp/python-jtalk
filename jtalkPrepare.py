869dc65e209cf882ac22be3faa2b4d6c40521e23
Takuya Nishimoto <nishimotz@gmail.com>
2022-03-26 17:17:48 +0900

    apply black formatter

--- jtalkPrepare.py
+++ jtalkPrepare.py
@@ -4,105 +4,120 @@
 
 import re
 import sys
+
 if sys.version_info.major >= 3:
-	re_ascii = re.ASCII
+    re_ascii = re.ASCII
 else:
-	re_ascii = 0
+    re_ascii = 0
 
 predic = None
 
+
 def setup():
-	global predic
-	if predic is None:
-		predic = load()
+    global predic
+    if predic is None:
+        predic = load()
+
 
 def convert(msg):
-	setup()
-	for p in predic:
-		try:
-			msg = re.sub(p[0], p[1], msg)
-		except:
-			pass
-	msg = msg.lower()
-	return msg
+    setup()
+    for p in predic:
+        try:
+            msg = re.sub(p[0], p[1], msg)
+        except:
+            pass
+    msg = msg.lower()
+    return msg
+
 
 def load():
-	return [
-		[re.compile(u'^ー$'), u'チョーオン'],
-		[re.compile(u'^ン$'), u'ウン'],
-		[re.compile(u'\\sー$'), u' チョーオン'],
-		[re.compile(u'\\sン$'), u' ウン'],
-
-		## 人々 昔々 家々 山々 
- 		[re.compile(u'(.)々'), u'\\1\\1'],
-
-		## isolated hiragana HA (mecab replaces to WA)
-		## は 
-		[re.compile(u'^は$'), u'ハ'],
-		[re.compile(u'\\sは$'), u' ハ'],
-		
-		## 59 名
-		[re.compile(u'(\\d) 名'), u'\\1名'],
-		## 4行 ヨンコー -> ヨンギョー
-		[re.compile(u'(\\d)行'), u'\\1ギョー'],
-		## 2 分前更新
-		[re.compile(u'(\\d)+ 分前更新'), u'\\1分マエコーシン'],
-		
-		## 1MB 10MB 1.2MB 0.5MB 321.0MB 123.45MB 2.7GB
-		## 1 MB 10 MB 1.2 MB 0.5 MB 321.0 MB 123.45 MB 2.7 GB
-		[re.compile(u'(\\d+)\\s*KB'), u'\\1キロバイト'],
-		[re.compile(u'(\\d+)\\s*MB'), u'\\1メガバイト'],
-		[re.compile(u'(\\d+)\\s*GB'), u'\\1ギガバイト'],
-		[re.compile(u'(\\d+)\\s*MHz'), u'\\1メガヘルツ'],
-		[re.compile(u'(\\d+)\\s*GHz'), u'\\1ギガヘルツ'],
-
-		## 2013 年 1 月 2 日
-		[re.compile(u'(\\d+)\\s+年\\s+(\\d+)\\s+月\\s+(\\d+)\\s+日'), u'\\1年\\2月\\3日'],
-
-		### zenkaku symbols convert
-		## ２０１１．０３．１１
-		## １，２３４円
-		[re.compile(u'．'), u'.'],
-		[re.compile(u'，'), u','],
-
-		## 1,234
-		## 1,234,567
-		## 1,234,567,890
-		## 1,23 = ichi comma niju san
-		## 1,0 = ichi comma zero
-		[re.compile(u'(\\d)\\,(\\d{3})'), u'\\1\\2'],
-		[re.compile(u'(\\d{2})\\,(\\d{3})'), u'\\1\\2'],
-		[re.compile(u'(\\d{3})\\,(\\d{3})'), u'\\1\\2'],
-		[re.compile(u'(\\d)\\,(\\d{1,2})'), u'\\1カンマ\\2'],
-
-		[re.compile(u'(\\d{1,4})\\.(\\d{1,4})\\.(\\d{1,4})\\.(\\d{1,4})'), u'\\1テン\\2テン\\3テン\\4'],
-		[re.compile(u'(\\d{1,4})\\.(\\d{1,4})\\.(\\d{1,4})'), u'\\1テン\\2テン\\3'],
-
-		# do not replace '0' after '.' to phonetic symbols (prepare)
-		[re.compile(u'\\.0'), u'.0\u00a0'],
-
-		[re.compile(u'\\b0(\\d)(\\d)(\\d)(\\d)(\\d)(\\d)(\\d)(\\d)(\\d)', re_ascii), u'  \u00a00  \u00a0\\1  \u00a0\\2  \u00a0\\3  \u00a0\\4  \u00a0\\5  \u00a0\\6  \u00a0\\7  \u00a0\\8  \u00a0\\9 '],
-		[re.compile(u'\\b0(\\d)(\\d)(\\d)(\\d)(\\d)(\\d)(\\d)(\\d)', re_ascii), u'  \u00a00  \u00a0\\1  \u00a0\\2  \u00a0\\3  \u00a0\\4  \u00a0\\5  \u00a0\\6  \u00a0\\7  \u00a0\\8 '],
-		[re.compile(u'\\b0(\\d)(\\d)(\\d)(\\d)(\\d)(\\d)(\\d)', re_ascii), u'  \u00a00  \u00a0\\1  \u00a0\\2  \u00a0\\3  \u00a0\\4  \u00a0\\5  \u00a0\\6  \u00a0\\7 '],
-		[re.compile(u'\\b0(\\d)(\\d)(\\d)(\\d)(\\d)(\\d)', re_ascii), u'  \u00a00  \u00a0\\1  \u00a0\\2  \u00a0\\3  \u00a0\\4  \u00a0\\5  \u00a0\\6 '],
-		[re.compile(u'\\b0(\\d)(\\d)(\\d)(\\d)(\\d)', re_ascii), u'  \u00a00  \u00a0\\1  \u00a0\\2  \u00a0\\3  \u00a0\\4  \u00a0\\5 '],
-		[re.compile(u'\\b0(\\d)(\\d)(\\d)(\\d)', re_ascii), u'  \u00a00  \u00a0\\1  \u00a0\\2  \u00a0\\3  \u00a0\\4 '],
-		[re.compile(u'\\b0(\\d)(\\d)(\\d)', re_ascii), u'  \u00a00  \u00a0\\1  \u00a0\\2  \u00a0\\3 '],
-		[re.compile(u'\\b0(\\d)(\\d)', re_ascii), u'  \u00a00  \u00a0\\1  \u00a0\\2 '],
-		[re.compile(u'\\b0(\\d)', re_ascii), u'  \u00a00  \u00a0\\1 '],
-
-		[re.compile(u' \u00a00'), u'ゼロ'],
-		[re.compile(u' \u00a01'), u'イチ'],
-		[re.compile(u' \u00a02'), u'ニー'],
-		[re.compile(u' \u00a03'), u'サン'],
-		[re.compile(u' \u00a04'), u'ヨン'],
-		[re.compile(u' \u00a05'), u'ゴー'],
-		[re.compile(u' \u00a06'), u'ロク'],
-		[re.compile(u' \u00a07'), u'ナナ'],
-		[re.compile(u' \u00a08'), u'ハチ'],
-		[re.compile(u' \u00a09'), u'キュー'],
-
-		# do not replace '0' after '.' to phonetic symbols (finalize)
-		[re.compile(u'\\.0\u00a0'), u'.0'],
-	]
-
+    return [
+        [re.compile("^ー$"), "チョーオン"],
+        [re.compile("^ン$"), "ウン"],
+        [re.compile("\\sー$"), " チョーオン"],
+        [re.compile("\\sン$"), " ウン"],
+        ## 人々 昔々 家々 山々
+        [re.compile("(.)々"), "\\1\\1"],
+        ## isolated hiragana HA (mecab replaces to WA)
+        ## は
+        [re.compile("^は$"), "ハ"],
+        [re.compile("\\sは$"), " ハ"],
+        ## 59 名
+        [re.compile("(\\d) 名"), "\\1名"],
+        ## 4行 ヨンコー -> ヨンギョー
+        [re.compile("(\\d)行"), "\\1ギョー"],
+        ## 2 分前更新
+        [re.compile("(\\d)+ 分前更新"), "\\1分マエコーシン"],
+        ## 1MB 10MB 1.2MB 0.5MB 321.0MB 123.45MB 2.7GB
+        ## 1 MB 10 MB 1.2 MB 0.5 MB 321.0 MB 123.45 MB 2.7 GB
+        [re.compile("(\\d+)\\s*KB"), "\\1キロバイト"],
+        [re.compile("(\\d+)\\s*MB"), "\\1メガバイト"],
+        [re.compile("(\\d+)\\s*GB"), "\\1ギガバイト"],
+        [re.compile("(\\d+)\\s*MHz"), "\\1メガヘルツ"],
+        [re.compile("(\\d+)\\s*GHz"), "\\1ギガヘルツ"],
+        ## 2013 年 1 月 2 日
+        [re.compile("(\\d+)\\s+年\\s+(\\d+)\\s+月\\s+(\\d+)\\s+日"), "\\1年\\2月\\3日"],
+        ### zenkaku symbols convert
+        ## ２０１１．０３．１１
+        ## １，２３４円
+        [re.compile("．"), "."],
+        [re.compile("，"), ","],
+        ## 1,234
+        ## 1,234,567
+        ## 1,234,567,890
+        ## 1,23 = ichi comma niju san
+        ## 1,0 = ichi comma zero
+        [re.compile("(\\d)\\,(\\d{3})"), "\\1\\2"],
+        [re.compile("(\\d{2})\\,(\\d{3})"), "\\1\\2"],
+        [re.compile("(\\d{3})\\,(\\d{3})"), "\\1\\2"],
+        [re.compile("(\\d)\\,(\\d{1,2})"), "\\1カンマ\\2"],
+        [
+            re.compile("(\\d{1,4})\\.(\\d{1,4})\\.(\\d{1,4})\\.(\\d{1,4})"),
+            "\\1テン\\2テン\\3テン\\4",
+        ],
+        [re.compile("(\\d{1,4})\\.(\\d{1,4})\\.(\\d{1,4})"), "\\1テン\\2テン\\3"],
+        # do not replace '0' after '.' to phonetic symbols (prepare)
+        [re.compile("\\.0"), ".0\u00a0"],
+        [
+            re.compile("\\b0(\\d)(\\d)(\\d)(\\d)(\\d)(\\d)(\\d)(\\d)(\\d)", re_ascii),
+            "  \u00a00  \u00a0\\1  \u00a0\\2  \u00a0\\3  \u00a0\\4  \u00a0\\5  \u00a0\\6  \u00a0\\7  \u00a0\\8  \u00a0\\9 ",
+        ],
+        [
+            re.compile("\\b0(\\d)(\\d)(\\d)(\\d)(\\d)(\\d)(\\d)(\\d)", re_ascii),
+            "  \u00a00  \u00a0\\1  \u00a0\\2  \u00a0\\3  \u00a0\\4  \u00a0\\5  \u00a0\\6  \u00a0\\7  \u00a0\\8 ",
+        ],
+        [
+            re.compile("\\b0(\\d)(\\d)(\\d)(\\d)(\\d)(\\d)(\\d)", re_ascii),
+            "  \u00a00  \u00a0\\1  \u00a0\\2  \u00a0\\3  \u00a0\\4  \u00a0\\5  \u00a0\\6  \u00a0\\7 ",
+        ],
+        [
+            re.compile("\\b0(\\d)(\\d)(\\d)(\\d)(\\d)(\\d)", re_ascii),
+            "  \u00a00  \u00a0\\1  \u00a0\\2  \u00a0\\3  \u00a0\\4  \u00a0\\5  \u00a0\\6 ",
+        ],
+        [
+            re.compile("\\b0(\\d)(\\d)(\\d)(\\d)(\\d)", re_ascii),
+            "  \u00a00  \u00a0\\1  \u00a0\\2  \u00a0\\3  \u00a0\\4  \u00a0\\5 ",
+        ],
+        [
+            re.compile("\\b0(\\d)(\\d)(\\d)(\\d)", re_ascii),
+            "  \u00a00  \u00a0\\1  \u00a0\\2  \u00a0\\3  \u00a0\\4 ",
+        ],
+        [
+            re.compile("\\b0(\\d)(\\d)(\\d)", re_ascii),
+            "  \u00a00  \u00a0\\1  \u00a0\\2  \u00a0\\3 ",
+        ],
+        [re.compile("\\b0(\\d)(\\d)", re_ascii), "  \u00a00  \u00a0\\1  \u00a0\\2 "],
+        [re.compile("\\b0(\\d)", re_ascii), "  \u00a00  \u00a0\\1 "],
+        [re.compile(" \u00a00"), "ゼロ"],
+        [re.compile(" \u00a01"), "イチ"],
+        [re.compile(" \u00a02"), "ニー"],
+        [re.compile(" \u00a03"), "サン"],
+        [re.compile(" \u00a04"), "ヨン"],
+        [re.compile(" \u00a05"), "ゴー"],
+        [re.compile(" \u00a06"), "ロク"],
+        [re.compile(" \u00a07"), "ナナ"],
+        [re.compile(" \u00a08"), "ハチ"],
+        [re.compile(" \u00a09"), "キュー"],
+        # do not replace '0' after '.' to phonetic symbols (finalize)
+        [re.compile("\\.0\u00a0"), ".0"],
+    ]
--- jtalkRunner.py
+++ jtalkRunner.py
@@ -18,139 +18,175 @@ import pstats
 from jtalkCore import *
 import jtalkPrepare
 
-JT_DIR = r'..\nvdajp\source\synthDrivers\jtalk'
-JT_LIB_DIR = r'.'
-JT_DLL = os.path.join(JT_LIB_DIR, 'libopenjtalk.dll')
+JT_DIR = r"..\nvdajp\source\synthDrivers\jtalk"
+JT_LIB_DIR = r"."
+JT_DLL = os.path.join(JT_LIB_DIR, "libopenjtalk.dll")
 
 voices = [
-	{"id": "V1",
-	 "name": "m1",
-	 "lang":"ja",
-	 "samp_rate": 48000,
-	 "fperiod": 240,
-	 "lf0_base":5.0,
-	 "speaker_attenuation":1.0,
-	 "htsvoice": os.path.join(JT_DIR, 'm001', 'm001.htsvoice'),
-	 #"espeak_variant": "max",
-	 },
-	{"id": "V2",
-	 "name": "mei",
-	 "lang":"ja",
-	 "samp_rate": 48000,
-	 "fperiod": 240,
-	 "lf0_base": 5.86,
-	 "pitch_bias": -10,
-	 "speaker_attenuation": 0.5,
-	 "htsvoice": os.path.join(JT_DIR, 'mei', 'mei_normal.htsvoice'),
-	 #"espeak_variant": "f1",
-	 },
-	{"id": "V3",
-	 "name": "lite",
-	 "lang":"ja",
-	 "samp_rate": 16000,
-	 "fperiod": 80,
-	 "lf0_base": 5.0,
-	 "pitch_bias": 0,
-	 "speaker_attenuation": 1.0,
-	 "htsvoice": os.path.join(JT_DIR, 'lite', 'voice.htsvoice'),
-	 #"espeak_variant": "max",
-	 },
-	]
+    {
+        "id": "V1",
+        "name": "m1",
+        "lang": "ja",
+        "samp_rate": 48000,
+        "fperiod": 240,
+        "lf0_base": 5.0,
+        "speaker_attenuation": 1.0,
+        "htsvoice": os.path.join(JT_DIR, "m001", "m001.htsvoice"),
+        # "espeak_variant": "max",
+    },
+    {
+        "id": "V2",
+        "name": "mei",
+        "lang": "ja",
+        "samp_rate": 48000,
+        "fperiod": 240,
+        "lf0_base": 5.86,
+        "pitch_bias": -10,
+        "speaker_attenuation": 0.5,
+        "htsvoice": os.path.join(JT_DIR, "mei", "mei_normal.htsvoice"),
+        # "espeak_variant": "f1",
+    },
+    {
+        "id": "V3",
+        "name": "lite",
+        "lang": "ja",
+        "samp_rate": 16000,
+        "fperiod": 80,
+        "lf0_base": 5.0,
+        "pitch_bias": 0,
+        "speaker_attenuation": 1.0,
+        "htsvoice": os.path.join(JT_DIR, "lite", "voice.htsvoice"),
+        # "espeak_variant": "max",
+    },
+]
 
-def pa_play(data, samp_rate = 16000):
-	import pyaudio
-	p = pyaudio.PyAudio()
-	stream = p.open(format = p.get_format_from_width(2),
-		channels = 1, rate = samp_rate, output = True)
-	size = len(data)
-	pos = 0 # byte count
-	while pos < size:
-		a = stream.get_write_available() * 2
-		o = data[pos:pos+a]
-		stream.write(o)
-		pos += a
-	time.sleep(float(size) / 2 / samp_rate)
-	stream.close()
-	p.terminate()
+
+def pa_play(data, samp_rate=16000):
+    import pyaudio
+
+    p = pyaudio.PyAudio()
+    stream = p.open(
+        format=p.get_format_from_width(2), channels=1, rate=samp_rate, output=True
+    )
+    size = len(data)
+    pos = 0  # byte count
+    while pos < size:
+        a = stream.get_write_available() * 2
+        o = data[pos : pos + a]
+        stream.write(o)
+        pos += a
+    time.sleep(float(size) / 2 / samp_rate)
+    stream.close()
+    p.terminate()
+
 
 def __print(s):
-	print(s.encode('cp932', 'ignore'))
+    print(s.encode("cp932", "ignore"))
+
 
 def print_code(msg):
-	s = ''
-	for c in msg:
-		s += '%04x ' % ord(c)
-	print(s)
+    s = ""
+    for c in msg:
+        s += "%04x " % ord(c)
+    print(s)
 
-def do_synthesis(msg, voice_args, do_play, do_write, do_write_jt, do_log, fperiod, pitch=50, inflection=50, vol=50):
-	msg = jtalkPrepare.convert(msg)
-	s = text2mecab(msg)
-	__print("utf-8: (%s)" % s.decode('utf-8', 'ignore'))
-	mf = MecabFeatures()
-	Mecab_analysis(s, mf)
-	Mecab_print(mf, __print)
-	Mecab_correctFeatures(mf)
-	ar = Mecab_splitFeatures(mf)
-	__print('array size %d' % len(ar))
-	max_level = int(326.67 * int(vol) + 100) # 100..32767
-	level =	int(max_level * voice_args['speaker_attenuation'])
-	lf0_amp = 0.020 * inflection # 50 = original range
-	ls = 0.015 * (pitch - 50.0 + voice_args['pitch_bias']) # 50 = no shift
-	lf0_offset = ls + voice_args['lf0_base'] * (1 - lf0_amp)
-	count = 0
-	for a in ar:
-		count += 1
-		__print('feature size %d' % a.size)
-		Mecab_print(a, __print)
-		Mecab_utf8_to_cp932(a)
-		if do_write_jt:
-			w = "_test%d.jt.wav" % count
-		else:
-			w = None
-		if do_log:
-			l = "_test%d.jtlog" % count
-		else:
-			l = None
-		data = libjt_synthesis(a.feature,
-							   a.size,
-							   begin_thres_=32,
-							   end_thres_=32,
-							   level_=level,
-							   fperiod_ = fperiod,
-							   lf0_offset_ = lf0_offset,
-							   lf0_amp_ = lf0_amp,
-							   logwrite_ = __print,
-							   jtlogfile_ = l,
-							   jtwavfile_ = w)
-		if data:
-			__print('data size %d' % len(data))
-			if do_play:
-				pa_play(data, samp_rate = voice_args['samp_rate'])
-			if do_write:
-				w = wave.Wave_write("_test%d.wav" % count)
-				w.setparams( (1, 2, voice_args['samp_rate'], len(data)/2,
-							  'NONE', 'not compressed') )
-				w.writeframes(data)
-				w.close()
-		libjt_refresh()
-		del a
-	del mf
 
-def main(do_play = False, do_write = True, do_write_jt = False, do_log = False, voice_id=1,s=''):
-	njd = NJD()
-	jpcommon = JPCommon()
-	engine = HTS_Engine()
-	libjt_initialize(JT_DLL)
-	v = voices[voice_id]
-	libjt_load(v['htsvoice'])
-	Mecab_initialize(__print, JT_DIR)
-	fperiod = v['fperiod']
-	do_synthesis(s, v, do_play, do_write, do_write_jt, do_log, fperiod, pitch=50, inflection=50)
+def do_synthesis(
+    msg,
+    voice_args,
+    do_play,
+    do_write,
+    do_write_jt,
+    do_log,
+    fperiod,
+    pitch=50,
+    inflection=50,
+    vol=50,
+):
+    msg = jtalkPrepare.convert(msg)
+    s = text2mecab(msg)
+    __print("utf-8: (%s)" % s.decode("utf-8", "ignore"))
+    mf = MecabFeatures()
+    Mecab_analysis(s, mf)
+    Mecab_print(mf, __print)
+    Mecab_correctFeatures(mf)
+    ar = Mecab_splitFeatures(mf)
+    __print("array size %d" % len(ar))
+    max_level = int(326.67 * int(vol) + 100)  # 100..32767
+    level = int(max_level * voice_args["speaker_attenuation"])
+    lf0_amp = 0.020 * inflection  # 50 = original range
+    ls = 0.015 * (pitch - 50.0 + voice_args["pitch_bias"])  # 50 = no shift
+    lf0_offset = ls + voice_args["lf0_base"] * (1 - lf0_amp)
+    count = 0
+    for a in ar:
+        count += 1
+        __print("feature size %d" % a.size)
+        Mecab_print(a, __print)
+        Mecab_utf8_to_cp932(a)
+        if do_write_jt:
+            w = "_test%d.jt.wav" % count
+        else:
+            w = None
+        if do_log:
+            l = "_test%d.jtlog" % count
+        else:
+            l = None
+        data = libjt_synthesis(
+            a.feature,
+            a.size,
+            begin_thres_=32,
+            end_thres_=32,
+            level_=level,
+            fperiod_=fperiod,
+            lf0_offset_=lf0_offset,
+            lf0_amp_=lf0_amp,
+            logwrite_=__print,
+            jtlogfile_=l,
+            jtwavfile_=w,
+        )
+        if data:
+            __print("data size %d" % len(data))
+            if do_play:
+                pa_play(data, samp_rate=voice_args["samp_rate"])
+            if do_write:
+                w = wave.Wave_write("_test%d.wav" % count)
+                w.setparams(
+                    (
+                        1,
+                        2,
+                        voice_args["samp_rate"],
+                        len(data) / 2,
+                        "NONE",
+                        "not compressed",
+                    )
+                )
+                w.writeframes(data)
+                w.close()
+        libjt_refresh()
+        del a
+    del mf
 
-if __name__ == '__main__':
-	main(do_play=False, do_write=True, do_log=True, voice_id=1, s='100.25ドル')
-	#prof = cProfile.run("main(do_play=True)", '_cprof.prof')
-	#p = pstats.Stats('_cprof.prof')
-	#p.strip_dirs()
-	#p.sort_stats('time', 'calls')
-	#p.print_stats()
+
+def main(
+    do_play=False, do_write=True, do_write_jt=False, do_log=False, voice_id=1, s=""
+):
+    njd = NJD()
+    jpcommon = JPCommon()
+    engine = HTS_Engine()
+    libjt_initialize(JT_DLL)
+    v = voices[voice_id]
+    libjt_load(v["htsvoice"])
+    Mecab_initialize(__print, JT_DIR)
+    fperiod = v["fperiod"]
+    do_synthesis(
+        s, v, do_play, do_write, do_write_jt, do_log, fperiod, pitch=50, inflection=50
+    )
+
+
+if __name__ == "__main__":
+    main(do_play=False, do_write=True, do_log=True, voice_id=1, s="100.25ドル")
+    # prof = cProfile.run("main(do_play=True)", '_cprof.prof')
+    # p = pstats.Stats('_cprof.prof')
+    # p.strip_dirs()
+    # p.sort_stats('time', 'calls')
+    # p.print_stats()
